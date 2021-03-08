b1b442d9675da6389b45d2498cb38f8f854aea24
acparkgh <acpark123@gmail.com>
2021-03-08 15:50:19 -0500

    complete Checkout

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burger.firebaseio.com/'
+    // baseURL: 'https://react-my-burger.firebaseio.com/'
+    baseURL: 'https://react-my-burger-14e2b-default-rtdb.firebaseio.com/'
 });
 
 export default instance;
