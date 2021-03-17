ad77c0880a8729cc49806dd09e9350ce1954dbf5
acparkgh <acpark123@gmail.com>
2021-03-17 18:28:31 -0400

    corrected redux-thunk import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'react-thunk';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
